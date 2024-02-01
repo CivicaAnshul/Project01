fdac0f46d1e929d55e09099e0811451b6f10a97d
gyan anveshan 1 vscode <gyananveshan1@gmail.com>
2024-02-01 13:03:44 +0530

    this is my first commit into newbranch2

---
+++ Java19Videos/src/session13/Classfortestingnewbranch.java
@@ -0,0 +1,10 @@
+package session13;
+
+public class Classfortestingnewbranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("God is Great");
+	}
+
+}
